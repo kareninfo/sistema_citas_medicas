196b62106a72b8ef21fa62c40354a6461d58d787
kareninfo <kmonzonquispe@gmail.com>
2025-12-05 21:19:17 -0500

    Agregada la app citas y configuradas URLs y settings

--- citasmedicas/urls.py
+++ citasmedicas/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sistema_citas project.
+URL configuration for citasmedicas project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('citas/', include('citas.urls')),
+    path('', RedirectView.as_view(url='/citas/')),  # redirige la raíz a /citas/
 ]
+
+
